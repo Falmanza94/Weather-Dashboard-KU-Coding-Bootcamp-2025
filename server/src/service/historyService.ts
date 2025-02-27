0f6a1d5dc6abc91cd5777955531e8b1683aabd1b
Falmanza94 <fischer.almanza@gmail.com>
2025-02-27 13:26:22 -0600

    a read method has been initialized and is now defined

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,9 +1,27 @@
+import fs from 'fs-extra';
 // TODO: Define a City class with name and id properties
+class City {
+  name: string;
+  id: string;
 
+  constructor(name: string, id: string) {
+    this.name = name;
+    this.id = id;
+  }
+}
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private path: string = './db/db.json';
+  private async read(): Promise<City[]> {
+      try {
+        const data = await fs.readJson(this.path);
+        return data as City[];
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
+    }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   // private async write(cities: City[]) {}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
